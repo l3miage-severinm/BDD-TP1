41c5c577215810272ddfbd353718da7e8494cb56
l3miage-severinm <155997926+l3miage-severinm@users.noreply.github.com>
2024-03-04 16:31:31 +0100

    Suppression d'annotations inutiles

--- server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -7,29 +7,22 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="bike")
 public class BikeEntity {
     @Id
     @Column(name = "immat",length = 8)
     private String immatriculation;
 
-    @Column(name = "capacity")
     private int capacity;
 
-    @Column(name = "cylinderNumber")
     @Enumerated(EnumType.STRING)
     private CylinderNumber cylinderNumber;
-
-    @Column(name = "automatic")
+    
     private boolean automatic;
 
-    @Column(name = "shifter")
     private boolean shifter;
 
-    @Column(name = "shifterType")
     @Enumerated(EnumType.ORDINAL)
     private ShifterType shifterType;
 
-    @Column(name = "circulationDate")
     private Date circulationDate;
 }
